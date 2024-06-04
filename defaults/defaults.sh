3225448024df884c427076ca963cae434fb4bc79
Robertas <accesspc@gmail.com>
2024-06-04 14:31:26 +0100

    feat: defaults - include cert-manager cli

--- defaults/defaults.sh
+++ defaults/defaults.sh
@@ -37,7 +37,14 @@ for i in $(seq 1 3) ; do echo -n "." ; sleep 1 ; done ; echo
 kubectl apply -f defaults/dashboard/
 
 # cert-manager
+echo -e "\n=> cert-manager cli\n"
+latest=$(curl -sL https://api.github.com/repos/cert-manager/cert-manager/releases/latest | jq -r '.tag_name')
+curl -fsSL -o ~/bin/cmctl https://github.com/cert-manager/cmctl/releases/latest/download/cmctl_linux_amd64
+chmod +x ~/bin/cmctl
+
 echo -e "\n=> cert-manager\n"
+# kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/${latest}/cert-manager.yaml
+# OR
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 helm upgrade --install \
@@ -45,6 +52,8 @@ helm upgrade --install \
   --namespace cert-manager \
   --create-namespace \
   --set installCRDs=true
+
+cmctl check api --wait=2m
 
 kubectl apply -f defaults/cert-manager/
 
